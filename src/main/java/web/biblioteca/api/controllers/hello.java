4780868245caab708a1838dc6f5fc27e5efe6984
sahandrade <92736761+sahandrade@users.noreply.github.com>
2023-09-16 01:41:10 -0300

    oi

--- src/main/java/web/biblioteca/api/controllers/hello.java
+++ src/main/java/web/biblioteca/api/controllers/hello.java
@@ -1,11 +1,13 @@
 package web.biblioteca.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("/api")
 public class hello {
     @GetMapping(value="/")
     public String getMethodName() {
